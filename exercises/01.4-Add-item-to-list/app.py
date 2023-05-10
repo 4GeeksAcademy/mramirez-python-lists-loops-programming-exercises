476510263e8f7226c19e90f16dbb61c1cd110133
Miguel Ramirez <121323094+mramirez092@users.noreply.github.com>
2023-05-10 01:40:54 +0000

    Ejercicios +

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -4,5 +4,7 @@ import random
 my_list = [4,5,734,43,45]
 
 #The magic is here:
-def random_nums():
-    return 
+for i in range(10):
+    my_list.append(random.randint(1, 100))
+
+print(my_list)
--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,3 +1,21 @@
 names = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 #Your code here:
+
+#1 OK
+names[1] = 'Steven'
+print(names)
+
+#2 OK
+names[10] = 'Pepe'
+print(names)
+
+#3 OK
+names[0] = names[2] + names[4]
+print(names)
+
+#4 
+last_index = len(names) - 1
+
+for i in reversed(range(len(names))):
+    print(names[i])
